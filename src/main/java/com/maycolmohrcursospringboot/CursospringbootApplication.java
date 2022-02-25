3c887b24afba039f6e7b12234c8f96b9f9c97e31
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-02-25 15:26:27 +0100

    Criando operacao de instanciacao

--- src/main/java/com/maycolmohrcursospringboot/CursospringbootApplication.java
+++ src/main/java/com/maycolmohrcursospringboot/CursospringbootApplication.java
@@ -1,13 +1,32 @@
 package com.maycolmohrcursospringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.maycolmohrcursospringboot.domain.Categoria;
+import com.maycolmohrcursospringboot.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringbootApplication {
+public class CursospringbootApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
 	}
 
 }
