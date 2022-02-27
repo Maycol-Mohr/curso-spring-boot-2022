6d859e52e8ff3507261f72f7c4a1d8f3a189a87a
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-02-27 16:57:56 +0100

    PUT, DELETE e GET para Cliente

--- src/main/java/com/maycolmohrcursospringboot/dto/ClienteDTO.java
+++ src/main/java/com/maycolmohrcursospringboot/dto/ClienteDTO.java
@@ -2,32 +2,34 @@ package com.maycolmohrcursospringboot.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
-import com.maycolmohrcursospringboot.domain.Categoria;
+import com.maycolmohrcursospringboot.domain.Cliente;
 
-public class CategoriaDTO implements Serializable {
+public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	
 	@NotEmpty(message = "Preenchimento obrigatório")
-	@Length(min=5, max=80, message = "O tamanho deve ser entre 5 e 80 caracteres!")
+	@Length(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres!")
 	private String nome;
 	
-	public CategoriaDTO() {
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido!")
+	private String email;
+	
+	public ClienteDTO() {
 	}
 	
-	
-
-	public CategoriaDTO(Categoria entity) {
+	public ClienteDTO(Cliente entity) {
 		id = entity.getId();
 		nome = entity.getNome();
+		email = entity.getEmail();
 	}
-
-
 
 	public Integer getId() {
 		return id;
@@ -43,5 +45,13 @@ public class CategoriaDTO implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
