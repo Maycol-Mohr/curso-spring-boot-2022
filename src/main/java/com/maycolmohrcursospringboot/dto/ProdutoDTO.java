9eddb336dd0f844dbd0cfd0dc0c89f09793a5185
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-02-28 15:26:52 +0100

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/maycolmohrcursospringboot/dto/ProdutoDTO.java
@@ -0,0 +1,48 @@
+package com.maycolmohrcursospringboot.dto;
+
+import java.io.Serializable;
+
+import com.maycolmohrcursospringboot.domain.Produto;
+
+public class ProdutoDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private Double preco;
+	
+	public ProdutoDTO() {
+	}
+	
+	
+
+	public ProdutoDTO(Produto entity) {
+		id = entity.getId();
+		nome = entity.getNome();
+		preco = entity.getPreco();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
