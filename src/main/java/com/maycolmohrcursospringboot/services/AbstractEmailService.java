68bda7b03235795e361ec58480f3b0bc9abbe26e
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-03-01 21:40:13 +0100

    Configuracao inicial do spring security

--- src/main/java/com/maycolmohrcursospringboot/services/AbstractEmailService.java
+++ src/main/java/com/maycolmohrcursospringboot/services/AbstractEmailService.java
@@ -7,6 +7,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -22,6 +23,7 @@ public abstract class AbstractEmailService implements EmailService {
 	
 	@Autowired
 	private TemplateEngine templateEngine;
+	
 	
 	@Autowired
 	private JavaMailSender javaMailSender;
